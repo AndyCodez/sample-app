a355ea1de67da7f2df6309fd5d072501d459dd2c
AndyCodez <andystudy4@gmail.com>
2016-06-07 11:06:51 +0300

    Add test for unsuccessful edit and properly handle the edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user- User.find(params[:id])
+    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       #Handle a successful update
     else
