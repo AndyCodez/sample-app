924f309ad772bda36c0be5938566d93ce1d7e60f
AndyCodez <andystudy4@gmail.com>
2016-06-10 12:14:19 +0300

    Create the proto-feed

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,10 +3,12 @@ class MicropostsController < ApplicationController
 	
 	def create
 		@micropost = current_user.microposts.build(micropost_params)
+
 		if @micropost.save
 			flash[:success] = "Micropost created!"
 			redirect_to root_url
 		else
+			@feed_items = []
 			render 'static_pages/home'
 		end
 	end
--- app/models/user.rb
+++ app/models/user.rb
@@ -40,4 +40,7 @@ class User < ActiveRecord::Base
 		update_attribute(:remember_digest, nil)
 	end
 
+	def feed
+		Micropost.where("user_id = ?", id)
+	end
 end
